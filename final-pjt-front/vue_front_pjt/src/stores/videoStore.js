a1a46d7589277459e0ba11a64724490d4c38311b
KimMinSeok <132821631+silence102@users.noreply.github.com>
2025-05-23 22:47:50 +0900

    Delete final-pjt-front/vue_front_pjt directory
    delete .env file

--- final-pjt-front/vue_front_pjt/src/stores/videoStore.js
+++
@@ -1,8 +0,0 @@
-// stores/videoStore.js
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
-
-export const useVideoStore = defineStore('video', () => {
-  const selectedVideo = ref(null)
-  return { selectedVideo }
-})
--- final-pjt-front/vue_front_pjt/src/views/AssetView.vue
+++
@@ -1,6 +0,0 @@
-<!-- AssetView.vue -->
-<template>
-  <div class="container mt-5">
-    <h1>현물 시세 차트 페이지입니다.</h1>
-  </div>
-</template>
